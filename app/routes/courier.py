db748cff5c3eb2be4dc1857727e1154670776504
Me <me@example.com>
2025-05-05 22:42:20 +0300

    add endpoint for courier navigation

--- app/routes/courier.py
+++ app/routes/courier.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from sqlmodel import Session, select
 from app.db import get_session
-from ..schemas import schema_user
+from ..schemas import (schema_user, schema_order)
 from sqlalchemy.exc import IntegrityError
 from psycopg2.errors import UniqueViolation
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
@@ -11,8 +11,11 @@ from app.config import settings
 from datetime import timedelta
 from typing import Annotated
 
+def distance(x1, y1, x2, y2):
+    return ((x1-x2)**2+(y1-y2)**2)**0.5
 
-router = APIRouter(prefix="/courier-status", tags=["Обновление состояния курьера"])
+router = APIRouter(prefix="/courier-status", 
+        tags=["Навигация и обновления состояния курьера"])
 
 @router.patch("/location", status_code=status.HTTP_200_OK)
 def update_location(x:float, y:float, 
@@ -40,4 +43,30 @@ def update_number(number: int,
         session.commit()
         session.refresh(current_courier)
         return current_courier
+
+@router.get("/", status_code=status.HTTP_200_OK,
+        response_model = schema_order.Order,
+        summary = "Получить цель")
+def get_target(current_courier: Annotated[schema_user.Courier,
+            Depends(auth_handler.get_current_courier)],
+            session: Session = Depends(get_session)):
+    orders = session.exec(select(schema_order.Order).where(
+        schema_order.Order.amount <= current_courier.current_number)).all()
+    if orders is None or len(orders) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail="No orders available. Return to the storehouse"
+        )
+    curr_x = current_courier.x
+    curr_y = current_courier.y
+    k = 0
+    min_d = distance(curr_x, curr_y, orders[0].x, orders[0].y)
+
+    for i in range(len(orders)):
+        t = distance(curr_x, curr_y, orders[i].x, orders[i].y)
+        if t < min_d:
+            min_d = t
+            k = i
+    return orders[k]
+        
 
